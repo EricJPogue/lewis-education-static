870f610fc849470f0ded3347031f5814fef88613
EricJPogue <epogue@epogue.com>
2022-03-29 10:42:45 -0500

    Update slides for SE session.

--- src/Slides/HotkeysDemo.js
+++ src/Slides/HotkeysDemo.js
@@ -1,8 +1,12 @@
-import React from 'react'
-import { useState } from 'react'
+import React, {useState } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook';
 
-export function MyComponent2() {
+// import { checklist } from './SLSprint00';
+import { ics6_1of6 } from './SLSprint06'
+
+
+/*
+function MyComponent2() {
 	const [count, setCount] = useState(0);
   
 	const ref = useHotkeys('b', () => setCount(prevCount =>
@@ -18,14 +22,103 @@ export function MyComponent2() {
 	);
   }
 
-  export function MyComponent() {
+  function MyComponent3() {
+	const incrementSlide = () => {
+		if (currentSlideNumber < (deck.length-1)) {
+			setCurrentSlideNumber(currentSlideNumber+1)
+		}
+	}
+
+	const decrementSlide = () => {
+		if (currentSlideNumber > 0) {
+			setCurrentSlideNumber(currentSlideNumber-1)
+		}
+	}
+
+	const [deckPlus, setDeckPlus] = useState(checklist())
+	const deck = checklist()
+
+	const [currentSlideNumber, setCurrentSlideNumber] = useState(0)
+
 	const [count, setCount] = useState(0);
+	const [countB, setCountB] = useState(1)
+
+	const [title, setTitle] = useState('state test')
   
-	useHotkeys('a', () => setCount(prevCount =>
-	  prevCount + 1
-	));
+	useHotkeys('a', () => setCount(prevCount => prevCount + 1 ));
+
+	useHotkeys('b', () => {
+		setTitle('title')
+		console.log('b='+useState.countB) 
+		setCountB(prevCount => prevCount+3)
+	})
   
-	return (
+	useHotkeys('c', () => {
+		incrementSlide()
+	})
+
+	useHotkeys('.', () => {
+		incrementSlide()
+	})
+
+	return ( <div>
 	  <span>{count}</span>
+		<h1>{title}</h1>
+
+		<span>{countB}</span>
+		{deckPlus[currentSlideNumber]()} 
+
+		<h2 style={{flex:1, position:"absolute", left:40, bottom:20}} onClick={() => decrementSlide()}>&larr;</h2>
+			<h2 style={{flex:1, position:"absolute", right:40, bottom:20}} onClick={() => incrementSlide()}>&rarr;</h2>
+	  </div>
+
 	);
+  }
+*/
+
+  export function MyComponent() {
+	const incrementSlide = () => {
+		console.log('incrementSlide currentSlideNumber='+currentSlideNumber)
+		if (currentSlideNumber < (deck.length-1)) {
+			setCurrentSlideNumber(currentSlideNumber+1)
+		}
+	}
+
+	const decrementSlide = () => {
+		console.log('decrementSlide currentSlideNumber='+currentSlideNumber)
+		if (currentSlideNumber > 0) {
+			setCurrentSlideNumber(currentSlideNumber-1)
+		}
+	}
+
+	const deck = ics6_1of6()
+
+	const [currentSlideNumber, setCurrentSlideNumber] = useState(0)
+  
+
+
+	useHotkeys('d', () => {
+		console.log('d')
+		console.log(currentSlideNumber)
+		if (currentSlideNumber > 0) {
+			setCurrentSlideNumber(currentSlideNumber-1)
+		}
+	})
+
+	useHotkeys('.', () => {
+		console.log('.')
+		console.log(currentSlideNumber)
+		if (currentSlideNumber < (deck.length-1)) {
+			setCurrentSlideNumber(currentSlideNumber+1)
+		}
+	})
+
+	return ( <div>
+
+<span>{currentSlideNumber}</span>
+		{deck[currentSlideNumber]()} 
+		<h2 style={{flex:1, position:"absolute", left:40, bottom:20}} onClick={() => decrementSlide()}>&larr;</h2>
+			<h2 style={{flex:1, position:"absolute", right:40, bottom:20}} onClick={() => incrementSlide()}>&rarr;</h2>
+	  </div>
+	)
   }
--- src/Slides/SLSprint06.js
+++ src/Slides/SLSprint06.js
@@ -5,13 +5,14 @@ export const sprint6Router = (route) => {
 	switch(route) {
 		case ICS_6_1of6_ROUTE: return ics6_1of6()
 		case OOP_6_1of6_ROUTE: return oop6_1of6()
+		case SE_6_1of4_ROUTE: return se6_1of4()
 		default: return null
 	}
 }
 
 // Session 1 of 6
 const ICS_6_1of6_ROUTE = 'ics6-1of6'
-const ics6_1of6 = () => {
+export const ics6_1of6 = () => {
 	const agenda = () => {
 		return agendaSlide([
 			'Prework',
@@ -45,4 +46,50 @@ const ics6_1of6 = () => {
 
 const OOP_6_1of6_ROUTE = 'oop6-1of6'
 const oop6_1of6 = () => { return ics6_1of6() }
+
+// Software Engineering
+const SE_6_1of4_ROUTE = 'se6-1of4'
+export const se6_1of4 = () => {
+	const agenda = () => {
+		return agendaSlide([
+			'Prework',
+			'Sprint 6 Planning - Class',
+			'Review Backlog Grooming',
+			'Sprint 6 Planning - Scrum Team',
+			'Prework for Next Class' ])
+	}
+	const preworkSE6_1of4 = () => {
+		return basicSlide('Prework', [
+			'All Sprint 5 Assignments due Sunday!', '',
+			'** Be prepared for in-person Sprint 6 Planning on Tuesday' ])
+	}
+	const sprintPlanning = () => {
+		return orderedListSlide('Sprint Planning', 'Sprint 6 Changes:', [
+			'Very similar to format to previous sprints with Discussion, Quiz, Lab, and Reflection',
+			'Completely new emphasis with our focus changing to your team delivering a product using agile development, scrum, and SaaS' ])
+	}
+	const activitiesReview = () => {
+		return basicSlide('Review Activity List and Assignments', [
+			'Please open Blackboard and review our activity list and assignments together.' ])
+	}
+	const prework6_2of4 = () => {
+		return basicSlide('Prework', [
+			'Complete through activity 5',
+			'Finalize and commit to your sprint 6 backlog', '',
+			'Be prepared for sprint 5 reviews (class demos of “Best Story” and “Random Story”) and retrospectives',
+			'Be prepared to complete Discussion 6'
+		])
+	}
+	const reviewBacklogGrooming = () => {
+		return basicSlide('Review Backlog Grooming', [
+			'Review our Backlog Groom activities and artifacts from Thursday’s class.']) 
+	}
+	const sprint6Planning = () => {
+		return basicSlide('Sprint 6 Planning', [
+			'Complete sprint 6 planning including final team and individual sprint 6 backlog commitments.']) 
+	}
+
+	return [ preflightChecklist, instructorChecklist, 
+		agenda, preworkSE6_1of4, sprintPlanning, activitiesReview, prework6_2of4, reviewBacklogGrooming, sprint6Planning, end ]
+}
 
