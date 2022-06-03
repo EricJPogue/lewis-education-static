a9df7d54c7134c93da873051eeaa0e474d71f1d9
EricJPogue <epogue@epogue.com>
2022-06-03 09:54:02 -0500

    Update files.

--- src/ActivityLists/AL24700Sprint05.js
+++ src/ActivityLists/AL24700Sprint05.js
@@ -8,7 +8,7 @@ import { mapManiaVersion2Link } from '../Activities/MapMania'
 export const al24700Sprint05 = () => {
 	const sprint = 5
 	return ( <div>
-		<h5>Sprint 5: User Experience, Advanced JavaScript, Final Project Proposals, and Relational Databases</h5>
+		<h5>Sprint 5: User Experience, Intermediate JavaScript, Final Project Proposals, and Databases</h5>
 		<p>Wow! We made it to the halfway point. I hope you enjoyed creating our initial version Map Mania. This sprint 
 		we are going to focus on enhancing Map Mania by creating version 2 of the product that implements the remaining
 		game playing features. It is impressive what you can create with a powerful application programming interface (API) 
@@ -18,7 +18,7 @@ export const al24700Sprint05 = () => {
 		{learningObjectivesIntro(sprint)}
 		<ul style={{listStyleType:'square'}}>
 			<li>Understand User Experience and how it relates to User Interface Design</li>
-			<li>Utilize advanced JavaScript to access Application Programming Interfaces (APIs)</li>
+			<li>Utilize Intermediate JavaScript to access Application Programming Interfaces (APIs)</li>
 			<li>Create sophisticated Web applications using the Google Maps API</li>
 			<li>Understand Relational Databases and the SQL language</li>
 		</ul>  
@@ -37,10 +37,11 @@ const list24700Sprint05 = (sprint) => {
 			<li>Complete {mapManiaVersion2Link()} requirement 5{estimated('6 hours')}</li>
 
 			{initialPost(sprint)}
-
 			<li>Complete {finalProjectProposalLink()}{estimated('2 hours')}</li>
-			{standardActivitiesClosing(sprint, mapManiaVersion2Link(), '6 hours' )}
+			{standardActivitiesClosing(sprint, mapManiaVersion2Link(), '6 hours')}
 		</ol>
 		{closing(sprint)}
 	</div> )
 }
+
+// Certified for SU22 (06/03/2022)
