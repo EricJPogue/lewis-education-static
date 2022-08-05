ab30b8717bd826d9edcd0dfa2604eeda49aca6c1
EricJPogue <epogue@epogue.com>
2022-08-05 14:23:59 -0500

    Update version to 003.

--- src/Info/Version.js
+++ src/Info/Version.js
@@ -3,6 +3,6 @@ import React from 'react'
 export const Version = () => {
 	return ( <div>
 		<h4>Lewis.education</h4>
-		<h5>Version 2022.fall.08.002</h5>
+		<h5>Version 2022.fall.08.003</h5>
 	</div> )
 }
