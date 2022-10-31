1c77bedd242506a624b9b0fb6230ee3f9e61b4f6
EricJPogue <epogue@epogue.com>
2022-10-31 09:41:17 -0500

    Update slides.

--- src/Slides/SL20000Sprint05.js
+++ src/Slides/SL20000Sprint05.js
@@ -3,9 +3,11 @@ import { getClass } from '../DataAndAPIs/Classes'
 import { xyz_n_1of6, xyz_n_1of6_lists } from './SL00000Sprint00'
 
 import { checklistAnnouncementsPreworkAndAgenda, completeDeck, tLab } from './SL00000Sprint00'
-import { tPrework, bulletListSlide, tDiscussionBreakout } from './SLSprint00'
+import { tPrework, bulletListSlide, tDiscussionBreakout, end } from './SLSprint00'
 
 import { submissionPercentage, retrospectiveIntroduction, orderedListSlide, retrospectiveBreakout, sprintDemos, sprintDemosIntro, demoAssignment } from './SLSprint00'
+
+import { breakoutStandard } from './SLSprint00'
 
 import { list20000Sprint04 } from '../ActivityLists/AL20000Sprint04'
 import { list20000Sprint05 } from '../ActivityLists/AL20000Sprint05'
@@ -124,3 +126,46 @@ const ics_5_4of6_lists = {
 		'Prework for Next Class',
 		'Lab & Programming Together' ]
 }
+export const ics_5_4of6 = () => {
+	const slideDeck = checklistAnnouncementsPreworkAndAgenda(ics_5_4of6_lists, sprint, activityList)
+	const breakout = () => { 
+		return breakoutStandard( 
+			'Breakout: Networks', 
+			'In this breakout session we will explore Networks:', [
+				'Network Types, Internet Connections, and Packet Switching',
+				'Open Systems, Network Protocols, TCP/IP, and High-Level Protocols... plus latency and bandwidth',
+				'The Web, HTTP, HTTPs, Encryption, and Domain Name System (DNS)',
+				'Cloud Computing and Web servers',
+				'Blockchain, Bitcoin, and Hashing... plus bitcoin vs ethereum energy consumption?' ]
+		)
+	}
+	const programmingTogether = () => {
+		return bulletListSlide('Lab & Programming Together', 
+			'How can we best make progress on TurtleDraw?', [
+			'Review the differences between TurtleDraw Lite and TurtleDraw',
+			'Implementing a README.md and LICENSE file in GitHub', 
+			'Added TurtleDraw.py file', 
+			'Pushed the updated files to GitHub',
+			'Start working on TurtleDraw' ])
+	}
+	const preworkNext = () => { return tPrework('Prework For Next Class', ics_4_5of6_lists.prework, sprint, activityList) }
+
+	return completeDeck(slideDeck, [ breakout, preworkNext, programmingTogether ])
+}
+
+// Sprint 5 session 5 of 6: Wednesday
+const ics_4_5of6_lists = {
+	'announcements':[ 
+		'Last warning that Friday, November 4 is the last day to withdraw from Fall 16-week classes with a grade of “W”',
+		'Any other announcements?' ],
+	'prework':[
+		'Complete through activity 11', '',
+		'Be prepared for Programming Together with Abstraction, Loops, Functions, and Files',
+		`Be prepared for Quiz ${sprint}` ],
+	'agenda':[
+		'Lab & Programming Together with Loops, Functions, and Files... but not Tuples, Lists, Dictionaries, and Sets',
+		'Quiz 4' ]
+}
+
+export const ics_5_5of6 = () => { return [end] }
+export const ics_5_6of6 = () => { return [end] }
