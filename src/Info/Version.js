11620bff37c0f1248308f37a25fd691539474419
EricJPogue <epogue@epogue.com>
2023-08-08 09:47:59 -0500

    Update files.

--- src/Info/Version.js
+++ src/Info/Version.js
@@ -3,6 +3,6 @@ import React from 'react'
 export const Version = () => {
 	return ( <div>
 		<h4>Lewis.education</h4>
-		<h5>Version 2022.fall.08.007</h5>
+		<h5>Version 2023.fall.08.001</h5>
 	</div> )
 }
