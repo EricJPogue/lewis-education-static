c0f51f75bf48d15a3ab2e3853d0eea9cc029bd0d
EricJPogue <epogue@epogue.com>
2023-09-10 11:03:01 -0500

    Update files.

--- src/Activities/StudyTable.js
+++ src/Activities/StudyTable.js
@@ -1,16 +1,11 @@
 
 import React from 'react';
-
-import StudyTableImage from './StudyTable.png';
+import StudyTableImage from './StudyTable2023.png';
 
 export const StudyTable = () => {
-	return (
-	
-<div>
-<div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-<img src={StudyTableImage} alt='Study Table' width= '1024'/>
-</div>
-
-</div> )
-
+	return ( <div>
+		<div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+		<img src={StudyTableImage} alt='Study Table' width= '1024'/>
+		</div>
+	</div> )
 }
