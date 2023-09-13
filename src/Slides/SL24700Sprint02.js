45dec80ae5b2d56413df319f2fbd6b3257bbfe51
EricJPogue <epogue@epogue.com>
2023-09-13 12:38:33 -0500

    Update files.

--- src/Slides/SL24700Sprint02.js
+++ src/Slides/SL24700Sprint02.js
@@ -4,6 +4,9 @@ import { list24700Sprint02 } from '../ActivityLists/AL24700Sprint02'
 import { xyz_n_1of6_lists, xyz_n_1of6 } from './SL00000Sprint00'
 import { xyz_2_1of6_sprintPlanning } from './SL00000Sprint02'
 
+import { checklistAnnouncementsPreworkAndAgenda, completeDeck, tReviewDemoSchedule, tLab } from './SL00000Sprint00'
+import { bulletListSlide, tDiscussionBreakout, tPrework } from './SLSprint00'
+
 //  Web & Distributed Programming (WEB) sprint 2 shared values.
 const sprint = 2
 const activityListPrevious = () => { return list24700Sprint01(sprint-1) }
@@ -28,4 +31,36 @@ const web_2_2of6_PAaA = {
 		'Confirm Demos for Next Class',
 		'Prework for Next Class',
 		'Lab & Programming Together' ]
+}
+
+export const web_2_2of6 = () => { 
+	const slideDeck = checklistAnnouncementsPreworkAndAgenda(web_2_2of6_PAaA, sprint, activityList)
+	const qAndA = () => { return bulletListSlide(`Sprint ${sprint} Planning Questions & Answers`, `What questions do you have about sprint ${sprint}?`, [ ]) }
+	const discussionBreakout = () => { return tDiscussionBreakout(sprint) } 
+	const preworkNext = () => { return tPrework('Prework For Next Class', web_2_3of6_PAaA.prework, sprint, activityList) }
+	const introducingDemos = () => {
+		return bulletListSlide('Foreshadowing Friday’s Sprint Demos', 
+			'Sprint demos are a key part of Agile software development and Scrum.', [
+			'They occur at the beginning of each new sprint ',
+			'They are an opportunity to show what was completed in the previous sprint',
+			'They are intended to be an **easy** and rewarding experience to show off your work',
+			'Also a chance to see how others solved a problem and to see some of the challenges they faces',
+			'We will each be doing at least on sprint demo during the semester during class' ])
+	}
+
+	return completeDeck(slideDeck, [ qAndA, discussionBreakout, introducingDemos, tReviewDemoSchedule, preworkNext, tLab ])
+}
+
+// Session 3 of 6: Friday
+const web_2_3of6_PAaA = {
+	'prework': [
+		'Complete through activity 9 prior to next class', '',
+		'Be prepared to discuss “The Information Layer”' ],
+	'announcements':[ 
+		'Any announcements?' ],
+	'agenda':[
+		`Sprint ${sprint-1} Demos`,
+		`Sprint ${sprint-1} Retrospective`,
+		`Breakout for Sprint ${sprint-1} Retrospective`,
+		'Prework for Next Class' ]
 }
