c2c7dde7058114dfbc6078ff89110d11ed1f1556
EricJPogue <epogue@epogue.com>
2023-09-23 13:28:10 -0500

    Update files.

--- src/ActivityLists/AL24700Sprint03.js
+++ src/ActivityLists/AL24700Sprint03.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import { learningObjectivesIntro, activitiesListIntro, standardActivities, initialPost, standardActivitiesClosing, 
-	closing } from './AL00000Sprint00'
+import { learningObjectivesIntro, activitiesListIntro, standardActivities, initialPost, standardActivitiesClosing, closing } from './AL00000Sprint00'
 
-import { oreillyPlaylistWeb, JavaScriptW3C, jsonArticle, jsonW3CTutorial, ajaxW3CTutorial, internalLink } from '../DataAndAPIs/Links'
+import { oreillyPlaylistWeb, JavaScriptW3C, jsonArticle, jsonW3CTutorial, ajaxW3CTutorial } from '../DataAndAPIs/Links'
 import { assignmentPortfolioLink } from '../Activities/WebAssignmentPortfolio'
 import { azureWebsiteLink } from '../Activities/AzureWebsite'
 
 import { lionsTigersAndBearsLink } from '../Activities/LionsTigerAndBears'
+import { mapManiaVersion1Link } from '../Activities/MapMania'
 
 export const al24700Sprint03 = () => {
 	const sprint = 3
@@ -33,20 +33,12 @@ export const al24700Sprint03 = () => {
 }
 
 export const list24700Sprint03 = (sprint) => {
-	const sebestaChapter4Lecture = () => { return internalLink('lecture', '/activity/sebesta-chapter-04') }
-	const sebestaChapter5Lecture = () => { return internalLink('lecture', '/activity/sebesta-chapter-05') }
-	const sebestaChapter6Lecture = () => { return internalLink('lecture', '/activity/sebesta-chapter-06') }
-	const sebestaChapter7Lecture = () => { return internalLink('lecture', '/activity/sebesta-chapter-07') }
-	const sebestaChapter10Lecture = () => { return internalLink('lecture', '/activity/sebesta-chapter-10') }
 	return ( <div>
 		{activitiesListIntro(sprint)}
 		<ol>
 			{standardActivities(sprint, lionsTigersAndBearsLink(), oreillyPlaylistWeb())}
 			<li>Continue to update and enhance your {assignmentPortfolioLink()}</li>
-
 			<li>Within {oreillyPlaylistWeb()} read “Learning Web Development” Chapters 4 on Interactivity while updating Amazeriffic</li>
-			<li>Scan Sebesta chapter 4 on Basic JavaScript and review the associated {sebestaChapter4Lecture()}</li>
-			<li>Briefly scan Sebesta chapter 5 &amp; 6 while reviewing the associated chapter 5 {sebestaChapter5Lecture()} &amp; chapter 6 {sebestaChapter6Lecture()} </li>
 			<li>Review {JavaScriptW3C()} tutorial from W3C</li>
 
 			{initialPost(sprint)}
@@ -54,14 +46,11 @@ export const list24700Sprint03 = (sprint) => {
 			<li>Read “Learning Web Development” Chapter 5 on JSON and AJAX while updating Amazeriffic</li>
 			<li>Utilize what we learned in {azureWebsiteLink()} to host your fully updated version of Amazeriffic</li>
 			<li>Review {jsonW3CTutorial()} and {ajaxW3CTutorial()} tutorials from W3C </li>
-			<li>Scan Sebesta chapter 7 &amp; 10 while reviewing the associated chapter 7 {sebestaChapter7Lecture()} &amp; chapter 10 {sebestaChapter10Lecture()} </li>
-
-			{standardActivitiesClosing(sprint, lionsTigersAndBearsLink(), '8 hours')}
+			<li>Preview {mapManiaVersion1Link()} and complete Requirement 1</li>
+			{standardActivitiesClosing(sprint, lionsTigersAndBearsLink(), '6 hours')}
 		</ol>
 	</div> )
 }
 
 // Certified for SU22 (05/20/2022)
-
-// Adding something like tic-tac-toe game tutorial.
-// Updating MapMania Lite to be more step-by-step including design. 
+// Certified for FA23 (09/23/2023)
