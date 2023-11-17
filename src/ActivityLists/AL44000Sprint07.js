b9f33aa3fe04053d13a59c08207166f9dfc27e5c
EricJPogue <epogue@epogue.com>
2023-11-17 12:26:22 -0600

    Update files.

--- src/ActivityLists/AL44000Sprint07.js
+++ src/ActivityLists/AL44000Sprint07.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { learningObjectivesIntro, activitiesListIntro, standardActivities, initialPost, standardActivitiesClosing, closing } from './AL00000Sprint00'
 
-import { AzureFunctionsWithVSCodeAndJavaScript } from '../DataAndAPIs/Links'
+// import { AzureFunctionsWithVSCodeAndJavaScript } from '../DataAndAPIs/Links'
 
 import { finalProjectSprint7Link } from '../Activities/FinalProject'
 import { introductionToReactLink } from '../Activities/IntroductionToReact'
@@ -13,16 +13,14 @@ export const al44000Sprint07 = () => {
 	const sprint = 7
 	return ( <div>
 		<h5>Sprint 7: {al44000Sprint7Topics}</h5>
-
-		<p>Welcome to sprint 7. This sprint will see us continuing to work on our final project, optimizing your team’s 
-		Agile processes, leaning about React, and looking at Design Patterns.</p>
+		<p>Welcome to sprint 7. This sprint will bring closure to our class project. We will be delivering stories, 
+		cleaning up our products, and transferring or accepting product ownership.</p>
 
 		{learningObjectivesIntro(sprint)}
 		<ul style={{listStyleType:'square'}}>
-			<li>Utilize your knowledge of Agile development and Saas to deliver sprint backlog stories</li>
+			<li>Utilize your knowledge of Agile development and SaaS to deliver sprint backlog stories</li>
 			<li>Understand Design Patterns with a particular focus on the Model-View-Controller pattern</li>
-			<li>Write serverless computing services utilizing Azure Functions</li>
-			<li>Delivery a basic React application in Azure</li>
+			<li>Understand basic React application in Azure</li>
 		</ul>
 
 		{list44000Sprint07(sprint)}
@@ -37,14 +35,12 @@ export const list44000Sprint07 = (sprint) => {
 		<ol>
 			{standardActivities(sprint, finalProjectSprint7Link(), oreillyPlaylistSE())}
 			<li>Read Chapter 10 of Fox on Project Management, Scrum, Pairs, and Version Control Systems</li>
-			<li>Within {oreillyPlaylistSE()} search for “Azure Functions” and “Serverless Computing” and document the best resources</li>
-			<li>Complete {AzureFunctionsWithVSCodeAndJavaScript()} and build your <em>personal</em> serverless function</li>
 
 			{initialPost(sprint)}
-			<li>Complete Introduction to React {introductionToReactLink()}</li>	
-			<li>Complete {reactProgrammingTogether()} programming together tutorial</li>
+			<li>Scan Introduction to React {introductionToReactLink()}</li>	
+			<li>Scan {reactProgrammingTogether()} programming together tutorial</li>
 
-			{standardActivitiesClosing(sprint, finalProjectSprint7Link(), '8 hours')}
+			{standardActivitiesClosing(sprint, finalProjectSprint7Link(), '20 hours')}
 		</ol>
 	</div> )
 }
@@ -53,4 +49,6 @@ export const list44000Sprint07 = (sprint) => {
 // Todo: Consider adding back in Architecture lecture or removing SQL questions from quiz.
 // Todo: Make sure that in sprints 7 & 8 that “individual” lab questions are identified. 
 // Todo: Review Zac’s quiz and update quiz 7. 
+// Todo: Consider '<li>Complete {AzureFunctionsWithVSCodeAndJavaScript()} and build your <em>personal</em> serverless function</li>'
+// Todo: Consider '<li>Within {oreillyPlaylistSE()} search for “Azure Functions” and “Serverless Computing” and document the best resources</li>'
 
