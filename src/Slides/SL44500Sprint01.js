937b88cb2ef3d54a64a94d2e8fa909895e5e7f6d
EricJPogue <epogue@epogue.com>
2024-01-24 06:45:20 -0600

    Update files.

--- src/Slides/SL44500Sprint01.js
+++ src/Slides/SL44500Sprint01.js
@@ -2,6 +2,8 @@
 import { checklistAnnouncementsPreworkAndAgenda, completeDeck, tLab } from "./SL00000Sprint00"
 import { tPrework } from "./SLSprint00"
 import { tRecap } from "./SLSprint00"
+import { tQuiz } from "./SLSprint00"
+import { breakoutStandard } from "./SLSprint00"
 
 import { list44500Sprint01 } from "../ActivityLists/AL44500Sprint01"
 import { sprint1_class_2_PAaA, sprint1_class_2 } from "./SL00000Sprint00"
@@ -42,10 +44,41 @@ const af_1_5of6_PAaA = {
 	'prework':[
 		'Complete through activity 10 prior to next class', '',
 		'Be prepared to complete Quiz 1', 
+		'Be prepared for “Object-Oriented Programming vs. Functional Programming” Breakout'],
+	'announcements':[ 
+		'Anyone have announcements they would like to share?' ],
+	'agenda':[ 
+		'Everything is due Sunday',
+		'Object-Oriented Programming vs. Functional Programming Breakout',
+		'Quiz 1' ]
+} 
+export const af_1_5of6 = () => {
+	const slideDeck = checklistAnnouncementsPreworkAndAgenda(af_1_5of6_PAaA, sprint, activityList)
+	const poll = () => { return tPrework('Sprint Progress Polling', af_1_5of6_PAaA.prework, sprint, activityList) }
+	const preworkNext = () => { return tPrework('Prework For Next Class', af_1_6of6_PAaA.prework, sprint, activityList) }
+	const recap = () => { return tRecap(af_1_5of6_PAaA.agenda.slice(1))}
+	const breakoutObjectOrientedVsFunctional = () => {
+		return breakoutStandard(
+			'Breakout: Object-Oriented Programming vs. Functional Programming', 
+			'In this breakout session on Object-Oriented Programming and Functional Programming your team will discuss:', [
+				'Object-Oriented Programming',
+				'Functional Programming', 
+				'Why would you choose one over the other?',
+				'Can they, and should they be used together?',
+				'Are we okay that we are going to be focused on Functional Programming in Application Frameworks?' ])
+	}
+
+	return completeDeck(slideDeck, [poll, preworkNext, breakoutObjectOrientedVsFunctional, recap, tQuiz])
+}
+
+// Session 6 of 6: Friday
+const af_1_6of6_PAaA = {
+	'prework':[
+		'Complete through activity 14 and working on activity 15 prior to next class', '', 
 		'Be prepared for Lab'],
 	'announcements':[ 
-		'' ],
+		'Anyone have announcements they would like to share?' ],
 	'agenda':[ 
-		'',
-		'' ]
-} 
+		'Everything is due Sunday',
+		'Lab' ]
+}
