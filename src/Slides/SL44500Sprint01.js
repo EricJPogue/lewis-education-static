ed5e48f083d1767fbeffc4314e43dae91571dd40
EricJPogue <epogue@epogue.com>
2024-01-25 18:12:31 -0600

    Update files.

--- src/Slides/SL44500Sprint01.js
+++ src/Slides/SL44500Sprint01.js
@@ -8,6 +8,8 @@ import { breakoutStandard } from "./SLSprint00"
 import { list44500Sprint01 } from "../ActivityLists/AL44500Sprint01"
 import { sprint1_class_2_PAaA, sprint1_class_2 } from "./SL00000Sprint00"
 
+import { xyz_n_1of6_lists } from "./SL00000Sprint00"
+
 // Application Frameworks sprint 1 global values.
 const sprint = 1
 const activityList = () => { return list44500Sprint01(sprint) }
@@ -52,21 +54,23 @@ const af_1_5of6_PAaA = {
 		'Object-Oriented Programming vs. Functional Programming Breakout',
 		'Quiz 1' ]
 } 
+
+const breakoutObjectOrientedVsFunctional = () => {
+	return breakoutStandard(
+		'Breakout: Object-Oriented Programming vs. Functional Programming', 
+		'In this breakout session on Object-Oriented Programming and Functional Programming your team will discuss:', [
+			'Object-Oriented Programming',
+			'Functional Programming', 
+			'Why would you choose one over the other?',
+			'Can they, and should they be used together?',
+			'Are we okay that we are going to be focused on Functional Programming in Application Frameworks?' ])
+}
 export const af_1_5of6 = () => {
 	const slideDeck = checklistAnnouncementsPreworkAndAgenda(af_1_5of6_PAaA, sprint, activityList)
 	const poll = () => { return tPrework('Sprint Progress Polling', af_1_5of6_PAaA.prework, sprint, activityList) }
 	const preworkNext = () => { return tPrework('Prework For Next Class', af_1_6of6_PAaA.prework, sprint, activityList) }
 	const recap = () => { return tRecap(af_1_5of6_PAaA.agenda.slice(1))}
-	const breakoutObjectOrientedVsFunctional = () => {
-		return breakoutStandard(
-			'Breakout: Object-Oriented Programming vs. Functional Programming', 
-			'In this breakout session on Object-Oriented Programming and Functional Programming your team will discuss:', [
-				'Object-Oriented Programming',
-				'Functional Programming', 
-				'Why would you choose one over the other?',
-				'Can they, and should they be used together?',
-				'Are we okay that we are going to be focused on Functional Programming in Application Frameworks?' ])
-	}
+
 
 	return completeDeck(slideDeck, [poll, preworkNext, breakoutObjectOrientedVsFunctional, recap, tQuiz])
 }
@@ -77,8 +81,21 @@ const af_1_6of6_PAaA = {
 		'Complete through activity 14 and working on activity 15 prior to next class', '', 
 		'Be prepared for Lab'],
 	'announcements':[ 
-		'Anyone have announcements they would like to share?' ],
+		'All sprint 1 assignments are due Sunday!' ],
 	'agenda':[ 
-		'Everything is due Sunday',
+		'Object-Oriented vs. Functional Breakout (continued)',
 		'Lab' ]
 }
+
+export const af_1_6of6 = () => {
+	const slideDeck = checklistAnnouncementsPreworkAndAgenda(af_1_6of6_PAaA, sprint, activityList)
+	const preworkNext = () => { return tPrework('Prework For Next Class', af_2_1of6_PAaA.prework, sprint, activityList) }
+	const recap = () => { return tRecap(af_1_5of6_PAaA.agenda.slice(1))}
+
+
+	return completeDeck(slideDeck, [ breakoutObjectOrientedVsFunctional, preworkNext, tLab, recap ])
+}
+
+// Sprint 2
+// Session 1 of 6
+const af_2_1of6_PAaA = xyz_n_1of6_lists(sprint)
