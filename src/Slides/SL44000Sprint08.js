2d79f2ccdfd1ced947e7eb6fbca5ec57116248b0
EricJPogue <epogue@epogue.com>
2024-05-06 14:53:26 -0500

    Update files.

--- src/Slides/SL44000Sprint08.js
+++ src/Slides/SL44000Sprint08.js
@@ -11,30 +11,34 @@ const activityList = () => { return list44000Sprint08(sprint) }
 // Final Exam / Final Project Presentation
 const se_8_7of6_PAaA = {
 	'announcements':[ 
-		'Final Project Presentations Today',
+		'Class Product Presentations',
 		'Please consider taking time to complete your course evaluation' ],
 	'prework':[
-		'Verify that you have submitted your final project presentation assignment',
 		'Be prepared for your final project presentations' ],
 	'agenda':[
-		'Final Project Presentations',
-		'Next Steps',
+		'Product Overview',
+		'Individual Class Product Presentations',
 		'Thank you!' ] 
 }
 export const se_8_7of6 = () => { 
 	const slideDeck = checklistAnnouncementsPreworkAndAgenda(se_8_7of6_PAaA, sprint, activityList)
-	const finalProjectPresentation = () => {
-		return orderedListSlide('Final Project Presentations', `Recall that your final project presentation should including a/an:`, [
-			'Brief introduction of yourself',
-			'Overview of your final project proposal',
-			'Demonstration standard and advanced features',
-			'Overview of the project source code',
-			'Description of what else you expect to complete by the end of the day Thursday' ])
+	const productOverview = () => {
+		return orderedListSlide(
+			'Class Product Presentations', `Product Overview (less than 5 minutes):`, [
+			'How would a user most likely utilize the product the first time they use it?',
+			'How regularly is the product being used currently?' ])
 	}
-	const nextSteps = () => { return tPrework('Next Steps', [ 'Everything is due Thursday!' ], sprint, activityList) }
+	const projectPresentations = () => {
+		return orderedListSlide(
+			'Class Product Presentations (3-4 minutes)', `Your class product presentation should including a/an:`, [
+			'Introduction yourself and what part of your project you enjoyed working on most',
+			'Demonstration of feature you are the most proud of delivering',
+			'Discussion of related source code',
+			'Description of what was transitioned to you or what you transitioned to someone else... is anything outstanding' ])
+	}
+	const nextSteps = () => { return tPrework('Next Steps', [ 'Everything is due Wednesday!' ], sprint, activityList) }
 	const finalComments = () => {
-		return orderedListSlide('Thank you!',
-			'', [
+		return orderedListSlide('Thank you!', '', [
 			'Thank you for making this a wonderful experience for me',
 			'Don’t hesitate to reach out in the future',
 			'Hope to see you in a future class', 
@@ -42,5 +46,5 @@ export const se_8_7of6 = () => {
 			'Very nice work! I am impressed with what you have accomplished.' ])
 	}
 
-	return completeDeck(slideDeck, [ finalProjectPresentation, nextSteps, finalComments ]) 
+	return completeDeck(slideDeck, [ productOverview, projectPresentations, nextSteps, finalComments ]) 
 }
